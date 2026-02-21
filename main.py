656514687e55045b5db38649ce7429bf7d728dcf
Asilbek <asildev96@gmail.com>
2026-02-21 12:47:45 +0500

    Change background color to black and adjust snake head initial position

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 
 window = turtle.Screen()
 window.title("Snake Game")
-window.bgcolor("white")
+window.bgcolor("black")
 window.setup(width=600, height=600)
 window.tracer(0)
 
@@ -19,7 +19,7 @@ head.speed(0)
 head.shape("square")
 head.color("green")
 head.penup()
-head.goto(0, 1)
+head.goto(0, 0)
 head.direction = "stop"
 
 
